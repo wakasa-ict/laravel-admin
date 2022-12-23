a8a2b83333110471a2e0e64e4019c8aff0605a03
kitamura <s.kitamura.biz@gmail.com>
2022-12-23 18:14:01 +0900

    マルチセレクトでセレクトのグループをサポート

--- resources/views/form/multipleselect.blade.php
+++ resources/views/form/multipleselect.blade.php
@@ -7,9 +7,20 @@
         @include('admin::form.error')
 
         <select class="form-control {{$class}}" style="width: 100%;" name="{{$name}}[]" multiple="multiple" data-placeholder="{{ $placeholder }}" {!! $attributes !!} >
-            @foreach($options as $select => $option)
-                <option value="{{$select}}" {{  in_array($select, (array)old($column, $value)) ?'selected':'' }}>{{$option}}</option>
-            @endforeach
+            @if($groups)
+                @foreach($groups as $group)
+                    <optgroup label="{{ $group['label'] }}">
+                        @foreach($group['options'] as $select => $option)
+                            <option value="{{$select}}" {{  in_array($select, (array)old($column, $value)) ?'selected':'' }}>{{$option}}</option>
+                        @endforeach
+
+                    </optgroup>
+                @endforeach
+            @else
+                @foreach($options as $select => $option)
+                    <option value="{{$select}}" {{  in_array($select, (array)old($column, $value)) ?'selected':'' }}>{{$option}}</option>
+                @endforeach
+            @endif
         </select>
         <input type="hidden" name="{{$name}}[]" />
 
