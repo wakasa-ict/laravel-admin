2e27b7f5ffa935eb38a3bb9bb1a8f51fd0cbbb36
kitamura <s.kitamura.biz@gmail.com>
2023-04-07 01:17:46 +0900

    管理者アバター画像管理を無効化

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -131,7 +131,6 @@ class AuthController extends Controller
 
         $form->display('username', trans('admin.username'));
         $form->text('name', trans('admin.name'))->rules('required');
-        $form->image('avatar', trans('admin.avatar'));
         $form->password('password', trans('admin.password'))->rules('confirmed|required');
         $form->password('password_confirmation', trans('admin.password_confirmation'))->rules('required')
             ->default(function ($form) {
